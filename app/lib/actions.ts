88982079fdb12fb32f8794a86907d77ca5db09c7
Roman Yanush <roman.yanush7@gmail.com>
2024-02-22 12:15:55 +0200

    feat: add update server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,13 @@
 'use server';
 
-import { z } from 'zod';
-import { sql } from '@vercel/postgres';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+import {z} from 'zod';
+import {sql} from '@vercel/postgres';
+import {revalidatePath} from 'next/cache';
+import {redirect} from 'next/navigation';
+
+/**
+ *
+ */
 
 const FormSchema = z.object({
     id: z.string(),
@@ -13,17 +17,45 @@ const FormSchema = z.object({
     date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const invoicesPath = '/dashboard/invoices';
 
+/**
+ *
+ */
+
+const CreateInvoice = FormSchema.omit({id: true, date: true});
+
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData.entries()));
+    const {customerId, amount, status} = CreateInvoice.parse(Object.fromEntries(formData.entries()));
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath(invoicesPath);
+    redirect(invoicesPath);
+}
+
+
+/**
+ *
+ */
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse(Object.fromEntries(formData.entries()));
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId},
+            amount      = ${amountInCents},
+            status      = ${status}
+        WHERE id = ${id}
     `;
 
     revalidatePath(invoicesPath);
